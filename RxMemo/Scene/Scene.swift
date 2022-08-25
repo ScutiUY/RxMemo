4aa0e48831eeeb4e3fd43cef803f5342054a9bb3
신의연 <dusdl14@gmail.com>
2022-08-25 19:46:36 +0900

    [feat] Scene Coordinator 구현

--- RxMemo/Scene/Scene.swift
+++ RxMemo/Scene/Scene.swift
@@ -5,4 +5,37 @@
 //  Created by 신의연 on 2022/08/25.
 //
 
-import Foundation
+import UIKit
+
+enum Scene {
+    case list(MemoListViewModel)
+    case detail(MemoDetailViewModel)
+    case compose(MemoComposeViewModel)
+}
+
+extension Scene {
+    func instatiate() -> UIViewController {
+        switch self {
+        case .list(let memoListViewModel):
+            var listVC = MemoListViewController()
+            let nav = UINavigationController(rootViewController: listVC)
+            listVC.bind(viewModel: memoListViewModel)
+            
+            return nav
+            
+        case .detail(let memoDetailViewModel):
+            var detailVC = MemoDetailViewController()
+            detailVC.bind(viewModel: memoDetailViewModel)
+            
+            return detailVC
+            
+        case .compose(let memoComposeViewModel):
+            var composeVC = MemoComposeViewController()
+            composeVC.bind(viewModel: memoComposeViewModel)
+            
+            return composeVC
+        }
+    }
+    
+}
+
--- RxMemo/Scene/TransitionModel.swift
+++ RxMemo/Scene/TransitionModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+enum TransitionStyle {
+    case root
+    case push
+    case modal
+}
+
+enum TransitionError: Error {
+    case navigationControllerMissing
+    case cannotPop
+    case unknown
+}
