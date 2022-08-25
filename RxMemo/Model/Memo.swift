1ba2a34b2d71bd3b72e5d2ec8345107cb81baa8c
신의연 <dusdl14@gmail.com>
2022-08-25 17:19:31 +0900

    [feat] create Memo Model

--- RxMemo/Model/Memo.swift
+++ RxMemo/Model/Memo.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+}
