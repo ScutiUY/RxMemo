0c05be23a89d3db540d19356b46d0ffb88c39175
신의연 <dusdl14@gmail.com>
2022-08-29 08:41:02 +0900

    [feat] ListTableView Cell 구현

--- RxMemo/ViewController/MemoListTableViewCell.swift
+++ RxMemo/ViewController/MemoListTableViewCell.swift
@@ -9,15 +9,35 @@ import UIKit
 
 class MemoListTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private var memoTitleLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "sample"
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: "cell")
+        setLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setLayout()
     }
+    
 
+    func setLayout() {
+        contentView.addSubview(memoTitleLabel)
+        
+        NSLayoutConstraint.activate([
+            memoTitleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            memoTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+        ])
+    }
+    
+    func fetchData(labelTitle: String) {
+        self.memoTitleLabel.text = labelTitle
+    }
+    
 }
