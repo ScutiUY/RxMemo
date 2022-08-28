3d4c888fac152ad63f4cf03e49381ac55e4ec734
신의연 <dusdl14@gmail.com>
2022-08-29 08:47:32 +0900

    [feat] listTableView 구현

--- RxMemo/ViewController/MemoListViewController.swift
+++ RxMemo/ViewController/MemoListViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MemoListViewController: UIViewController, ViewModelBindableType {
+    
+    private let bag = DisposeBag()
     
     var viewModel: MemoListViewModel!
     
@@ -15,11 +19,43 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
         
     }
     
+    private var memoListTableView: UITableView = {
+        var tableView = UITableView()
+        tableView.register(MemoListTableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "MemoList"
-        self.view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        setLayout()
+        setNaviButton()
     }
+    
+    private func setLayout() {
+        self.view.backgroundColor = .systemBackground
+        
+        view.addSubview(memoListTableView)
+        
+        NSLayoutConstraint.activate([
+            memoListTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            memoListTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            memoListTableView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            memoListTableView.widthAnchor.constraint(equalTo: view.widthAnchor),
+        ])
+    }
+    
+    private func setNaviButton() {
+        let addMemoButton = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+        self.navigationItem.rightBarButtonItem = addMemoButton
+        
+        addMemoButton.rx.tap
+            .bind {
+                let composeVC = MemoComposeViewController()
+                self.navigationController?.pushViewController(composeVC, animated: true)
+            }.disposed(by: bag)
+    }
+    
+    
 
 }
