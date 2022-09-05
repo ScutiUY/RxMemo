c6d5201ea07599495fe30a034242cc29e64f60e9
ScutiUY <scutiuys@gmail.com>
2022-09-05 22:11:25 +0900

    [feat] contentTableViewCell 생성

--- RxMemo/ViewController/MemoDateTableViewCell.swift
+++ RxMemo/ViewController/MemoDateTableViewCell.swift
@@ -9,15 +9,37 @@ import UIKit
 
 class MemoDateTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private var dateLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "sample"
+        label.textColor = .secondaryLabel
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: "cell")
+        setLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setLayout()
     }
+    
 
+    func setLayout() {
+        contentView.addSubview(dateLabel)
+        
+        NSLayoutConstraint.activate([
+            dateLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            dateLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+        ])
+    }
+    
+    func fetchData(labelTitle: String) {
+        self.dateLabel.text = labelTitle
+    }
+    
 }
