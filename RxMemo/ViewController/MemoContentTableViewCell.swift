eed1ed843c27bc889a5805bd90a498f2c2f6377e
ScutiUY <scutiuys@gmail.com>
2022-09-05 22:10:59 +0900

    [feat] 메모 상세보기 contentTableViewCell, dateCell 추가

---
+++ RxMemo/ViewController/MemoContentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoContentTableViewCell.swift
+//  RxMemo
+//
+//  Created by 신의연 on 2022/09/05.
+//
+
+import UIKit
+
+class MemoContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
